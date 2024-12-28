7f1bd901ae1c134a60afffc26f3000e9fd380faa
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-12-28 10:00:47 -0500

    Colocando el boton de Editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import {Activity} from "../types";
 import {categories} from "../data/categories.ts";
 import {useMemo} from "react";
+import { PencilSquareIcon } from '@heroicons/react/24/outline'
 
 type ActivityListProps = {
     activities: Activity[]
@@ -16,7 +17,7 @@ const ActivityList = ({activities}: ActivityListProps) => {
         <>
             <h2 className='text-4xl font-bold text-slate-600 text-center'>Comida y Actividades</h2>
             {activities.map((activity) => (
-                <div key={activity.id} className='bg-white shadow p-5 flex justify-between rounded-xl mt-5'>
+                <div key={activity.id} className='bg-white shadow px-5 py-10 flex justify-between rounded-xl mt-5'>
                     <div className='space-y-2 relative'>
                         <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'} rounded-xl`}>
                             {categoryName(+activity.category)}
@@ -29,8 +30,12 @@ const ActivityList = ({activities}: ActivityListProps) => {
                             <span className='text-slate-500 text-2xl'>Calorías</span>
                         </p>
                     </div>
-                    <div>
-
+                    <div className='flex gap-5 items-center'>
+                        <button>
+                            <PencilSquareIcon
+                                className='h-8 w-8 text-gray-800'
+                            />
+                        </button>
                     </div>
                 </div>
             ))}

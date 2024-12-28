bed0e604088146187261abdb587ed422ecb437d1
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-12-28 10:13:03 -0500

    Creando accion para editar y eliminar

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -1,13 +1,17 @@
 import {Activity} from "../types";
 
 export type ActivityActions =
-    { type: 'ADD_ACTIVITY', payload: { newActivity: Activity } }
+    { type: 'ADD_ACTIVITY', payload: { newActivity: Activity } } |
+    { type: 'DELETE_ACTIVITY', payload: { id: Activity['id'] } } |
+    { type: 'SET_ACTIVE_ACTIVITY', payload: { id: Activity['id'] } }
 
 type ActivityState = {
-    activities: Activity[]
+    activities: Activity[],
+    activeId: Activity['id']
 }
 export const initialState: ActivityState = {
-    activities: []
+    activities: [],
+    activeId: ''
 }
 export const activityReducer = (state: ActivityState = initialState, action: ActivityActions): ActivityState => {
 
@@ -16,6 +20,16 @@ export const activityReducer = (state: ActivityState = initialState, action: Act
             return {
                 ...state,
                 activities: [...state.activities, action.payload.newActivity]
+            }
+        case 'DELETE_ACTIVITY':
+            return {
+                ...state,
+                activities: state.activities.filter(activity => activity.id !== action.payload.id)
+            }
+        case 'SET_ACTIVE_ACTIVITY':
+            return {
+                ...state,
+                activeId: action.payload.id
             }
         default:
             return state
